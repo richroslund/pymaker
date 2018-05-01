8e1d70fdb1ce882000413b7ad8f4c32189f631ef
reverendus <reverendus@invalid>
2018-04-30 19:42:08 -0500

    `Flapper` wrapper stub

---
+++ pymaker/auctions.py
@@ -0,0 +1,69 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2017-2018 reverendus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from web3 import Web3
+
+from pymaker import Contract, Address, Transact
+
+
+class Flapper(Contract):
+    """A client for the `Flapper` contract, TODO.
+
+    You can find the source code of the `Flapper` contract here:
+    <TODO>.
+
+    Attributes:
+        web3: An instance of `Web` from `web3.py`.
+        address: Ethereum address of the `Flapper` contract.
+    """
+
+    abi = Contract._load_abi(__name__, 'abi/Flapper.abi')
+    bin = Contract._load_bin(__name__, 'abi/Flapper.bin')
+
+    @staticmethod
+    def deploy(web3: Web3, pie: Address, gem: Address):
+        assert(isinstance(pie, Address))
+        assert(isinstance(gem, Address))
+
+        return Flapper(web3=web3, address=Contract._deploy(web3, Flapper.abi, Flapper.bin, [pie.address, gem.address]))
+
+    def __init__(self, web3: Web3, address: Address):
+        assert(isinstance(web3, Web3))
+        assert(isinstance(address, Address))
+
+        self.web3 = web3
+        self.address = address
+        self._contract = self._get_contract(web3, self.abi, address)
+
+    def pie(self) -> Address:
+        """Returns the `pie` token.
+
+        Returns:
+            The address of the `pie` token.
+        """
+        return Address(self._contract.call().pie())
+
+    def gem(self) -> Address:
+        """Returns the `gem` token.
+
+        Returns:
+            The address of the `gem` token.
+        """
+        return Address(self._contract.call().gem())
+
+    def __repr__(self):
+        return f"Flapper('{self.address}')"
---
+++ tests/test_auctions.py
@@ -0,0 +1,38 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2017-2018 reverendus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from web3 import Web3, EthereumTesterProvider
+
+from pymaker import Address
+from pymaker.auctions import Flapper
+from pymaker.token import DSToken
+
+
+class TestFlapper:
+    def setup_method(self):
+        self.web3 = Web3(EthereumTesterProvider())
+        self.web3.eth.defaultAccount = self.web3.eth.accounts[0]
+        self.our_address = Address(self.web3.eth.defaultAccount)
+        self.pie = DSToken.deploy(self.web3, 'DAI')
+        self.gem = DSToken.deploy(self.web3, 'MKR')
+        self.flapper = Flapper.deploy(self.web3, self.pie.address, self.gem.address)
+
+    def test_pie(self):
+        assert self.flapper.pie() == self.pie.address
+
+    def test_gem(self):
+        assert self.flapper.gem() == self.gem.address
